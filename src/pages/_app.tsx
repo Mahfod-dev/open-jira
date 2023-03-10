1aece1b86af74832e25dd89359c7973ffd3f825d
Mahfod <mahfod.dev@gmail.com>
2023-03-10 20:54:22 +0100

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
+import { ThemeProvider, createTheme,CssBaseline } from '@mui/material'
 import type { AppProps } from 'next/app'
+import { darkTheme,lightTheme } from '@/themes';
+
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={darkTheme}>
+    <CssBaseline/>
+		<Component {...pageProps} />
+	</ThemeProvider>;
 }
