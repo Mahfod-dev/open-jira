3da4d5c00adc2ba1f6caafc589ef15f0ed0ee2a2
Mahfod <mahfod.dev@gmail.com>
2023-03-11 19:57:29 +0100

    create card home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,35 @@
 import { Layout } from '@/components/layouts';
-import { Typography } from '@mui/material';
+import { Typography, Grid, CardHeader, Card, CardContent } from '@mui/material';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-	return <Layout>
+	return (
+		<Layout title='Home - OpenJira'>
+			<Grid container spacing={2}>
+				<Grid item xs={12} sm={4}>
+					<Card sx={{ height: 'calc(100vh - 100px)' }}>
+						<CardHeader title='Pending' />
 
-    <Typography variant='h1' color='primary'>Hello world</Typography>
-
-  </Layout>;
+          <CardContent>
+            {/* nouvelle tache */}
+            {/* liste d'entre */}
+          </CardContent>
+          
+					</Card>
+				</Grid>
+				<Grid item xs={12} sm={4}>
+					<Card sx={{ height: 'calc(100vh - 100px)' }}>
+						<CardHeader title='In Progress' />
+					</Card>
+				</Grid>
+				<Grid item xs={12} sm={4}>
+					<Card sx={{ height: 'calc(100vh - 100px)' }}>
+						<CardHeader title='Completed' />
+					</Card>
+				</Grid>
+			</Grid>
+		</Layout>
+	);
 }
